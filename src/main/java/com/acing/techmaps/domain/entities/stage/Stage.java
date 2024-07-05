02d731db949f92181ee8e4114bd45b73162b14b9
yellowisk <oheitoralmeida2013@gmail.com>
2024-07-05 20:45:32 -0300

    feat: add Stage java entity

---
+++ src/main/java/com/acing/techmaps/domain/entities/stage/Stage.java
@@ -0,0 +1,34 @@
+package com.acing.techmaps.domain.entities.stage;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.UUID;
+
+@Getter
+@Setter
+public class Stage {
+    private UUID id;
+    private UUID roadmapId;
+    private String name;
+
+    public Stage(UUID id, UUID roadmapId, String name) {
+        this.id = id;
+        this.roadmapId = roadmapId;
+        this.name = name;
+    }
+
+    public Stage(UUID roadmapId, String name) {
+        this.roadmapId = roadmapId;
+        this.name = name;
+    }
+
+    public static Stage createFull(UUID id, UUID roadmapId, String name) {
+        return new Stage(id, roadmapId, name);
+    }
+
+    public static Stage fromRequest(UUID roadmapId, String name) {
+        return new Stage(roadmapId, name);
+    }
+
+}
