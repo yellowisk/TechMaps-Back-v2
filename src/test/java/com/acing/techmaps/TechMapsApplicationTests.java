21d43787263708ab12382548bc727e5134b02e60
yellowisk <oheitoralmeida2013@gmail.com>
2024-07-23 14:05:56 -0300

    test: add SpringBootTest

---
+++ src/test/java/com/acing/techmaps/TechMapsApplicationTests.java
@@ -0,0 +1,12 @@
+package com.acing.techmaps;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class TechMapsApplicationTests {
+
+        @Test
+        void contextLoads() {
+        }
+}
