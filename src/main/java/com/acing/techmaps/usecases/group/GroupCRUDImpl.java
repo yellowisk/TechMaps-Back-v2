70b3e910f3c23fb151eb85ca82207a90c543932e
yellowisk <oheitoralmeida2013@gmail.com>
2024-12-03 17:49:57 -0300

    doc: add TODOS for the creation of Groups

--- src/main/java/com/acing/techmaps/usecases/group/GroupCRUDImpl.java
+++ src/main/java/com/acing/techmaps/usecases/group/GroupCRUDImpl.java
@@ -1,6 +1,8 @@
 package com.acing.techmaps.usecases.group;
 
 import com.acing.techmaps.domain.entities.group.Group;
+import com.acing.techmaps.domain.entities.user.User;
+import com.acing.techmaps.security.utils.SecurityUtils;
 import com.acing.techmaps.usecases.group.gateway.GroupDAO;
 import com.acing.techmaps.web.model.group.request.GroupRequest;
 import lombok.AllArgsConstructor;
@@ -12,9 +14,16 @@ import java.util.UUID;
 @Service
 public class GroupCRUDImpl implements GroupCRUD {
     private final GroupDAO groupDAO;
+    private final SecurityUtils securityUtils;
 
     @Override
     public Group create(GroupRequest request) {
+        User user = securityUtils.getCurrentUser();
+        /*TODO: figure out way of find the parent group of the new group...
+         There are cases where this new group may be a sub-group of another group
+        */
+        //TODO: check if the current user has permission to create a group through the GroupUserDAO
+        //TODO: find out where to put all this logic
         return groupDAO.add(request.toGroup());
     }
 
